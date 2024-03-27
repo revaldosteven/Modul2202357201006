8ed64dd77837be6455b82c80488d5281ab1217c2
revalsteve <revaldosteven1986@gmail.>
2024-03-27 09:43:52 +0700

    First Commit

---
+++ src/modul2202357201006/Modul2202357201006.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul2202357201006;
+
+/**
+ *
+ * @author REVALDO STEVEN
+ */
+public class Modul2202357201006 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
